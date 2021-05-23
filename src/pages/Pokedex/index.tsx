70dc83a735eb30c6b3fe7013b7525948735371ef
Denis Korzun <dzianiskor@gmail.com>
2021-05-23 18:23:33 +0300

    homework-5

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -3,30 +3,35 @@ import PokemonCard from '../../components/PokemonCard';
 import s from './Pokedex.module.scss';
 import req from '../../utils/request';
 
-// interface IPokemon {
-//   name_clean?: string;
-//   abilities?: string[];
-//   stats: {
-//     hp: number;
-//     attack: number;
-//     defense: number;
-//     'special-attack': number;
-//     'special-defense': number;
-//     speed?: number;
-//   };
-//   types: string[];
-//   img: string;
-//   name: string;
-//   base_experience?: number;
-//   height?: number;
-//   id: number;
-//   is_default?: boolean;
-//   order?: number;
-//   weight?: number;
-// }
+interface IPokemon {
+  name_clean?: string;
+  abilities?: string[];
+  stats: {
+    hp: number;
+    attack: number;
+    defense: number;
+    'special-attack': number;
+    'special-defense': number;
+    speed?: number;
+  };
+  types: string[];
+  img: string;
+  name: string;
+  base_experience?: number;
+  height?: number;
+  id: number;
+  is_default?: boolean;
+  order?: number;
+  weight?: number;
+}
+
+interface IPokemonData {
+  total: string;
+  pokemons: IPokemon[];
+}
 
 const usePokemons = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<IPokemonData | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   useEffect(() => {
@@ -62,11 +67,10 @@ const PokedexPage: React.FC = () => {
     return <div>Something went wrong!</div>;
   }
 
-  // @ts-ignore
   return (
     <div className={s.root}>
       <div className={s.title}>
-        {data.total} <b>Pokemons</b> for you to choose your favorite
+        {data?.total} <b>Pokemons</b> for you to choose your favorite
       </div>
       <div className={s.searchFieldWrapper}>
         <input className={s.searchField} type="text" placeholder="Encuentra tu pokémon..." />
@@ -83,7 +87,7 @@ const PokedexPage: React.FC = () => {
         </button>
       </div>
       <div className={s.cardWrapper}>
-        {data.pokemons.map((pokemon) => (
+        {data?.pokemons.map((pokemon) => (
           <PokemonCard
             name={pokemon.name}
             attack={pokemon.stats.attack}
--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -3,7 +3,7 @@ import config from '../config';
 function getUrlWithParamsConfig(endpointConfig: string) {
   return {
     ...config.client.server,
-    ...config.client.endpoint[endpointConfig].uri,
+    ...config.client.endpoint[endpointConfig as keyof typeof config.client.endpoint].uri,
   };
 }
 
