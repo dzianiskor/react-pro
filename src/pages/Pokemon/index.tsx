c0aebdacc316ed60798751f7bc949800efcaf43b
Denis Korzun <dzianiskor@gmail.com>
2021-05-26 19:05:07 +0300

    upgrade project

---
+++ src/pages/Pokemon/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface PokemonProps {
+    id: string | number;
+}
+
+const Pokemon: React.FC<PokemonProps> = ({id}) => {
+    return (
+        <div>
+            This is pokemon {id}
+        </div>
+    );
+};
+
+export default Pokemon;
